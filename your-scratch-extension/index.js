3e6ee4ae42192841804b1e7a3517af8fb8c6637c
kodebeta <134710080+kodebeta@users.noreply.github.com>
2024-02-20 12:34:01 +0000

    Update

--- your-scratch-extension/index.js
+++ your-scratch-extension/index.js
@@ -14,84 +14,72 @@ class Scratch3YourExtension {
     getInfo () {
         return {
             // unique ID for your extension
-            id: 'Testing',
-        
-            // name displayed in the Scratch UI
-            name: 'Testing',      
+            id: 'TestingExtension',
+
+            // name that will be displayed in the Scratch UI
+            name: 'Testing',
+
             // colours to use for your extension blocks
             color1: '#800000',
             color2: '#fff700',
-        
+
             // your Scratch blocks
             blocks: [
-                {
-                    // function where your code logic lives
-                    opcode: 'assert',
+              {
+                // function where your code logic lives
+                opcode: 'assert',
+        
+                // type of block
+                blockType: BlockType.BOOLEAN,
+        
+                // label to display on the block
+                text: 'assert [assert]',
+        
+                // arguments used in the block
+                arguments: {
+                  assert:{
+                    type: ArgumentType.BOOLEAN,
+                    defaultValue: false
+                  }
+                }
+              },
+              {
+                // function where your code logic lives
+                opcode: 'inspect',
             
-                    // type of block
-                    blockType: BlockType.BOOLEAN,
-            
-                    // label to display on the block
-                    text: 'assert [assert]',
-            
-                    // arguments used in the block
-                    arguments: {
-                        assert:{
-                            type: ArgumentType.BOOLEAN,
-                            defaultValue: false // Changed to boolean
-                        }
-                    }
-                },
-                {
-                    // function where your code logic lives
-                    opcode: 'inspect',
-            
-                    // type of block
-                    blockType: BlockType.REPORTER,
-            
-                    // label to display on the block
-                    text: 'inspect [inspect]',
-            
-                    // arguments used in the block
-                    arguments: {
-                        inspect:{
-                            type: ArgumentType.NUMBER,
-                            defaultValue: 0
-                        }
+                // type of block
+                blockType: BlockType.REPORTER,
+        
+                // label to display on the block
+                text: 'inspect [inspect]',
+        
+                // arguments used in the block
+                arguments: {
+                    inspect:{
+                        type: ArgumentType.NUMBER,
+                        defaultValue: 0
                     }
                 }
-            ]  
+              }
+				
+            ]
         };
     }
+
 
     /**
      * implementation of the block with the opcode that matches this name
      *  this will be called when the block is used
      */
-    command1(args) {
-        this.text = Cast.toString(args.TEXT);
-    }
-/*
-    assert(args) {
-        const { assert } = args;
-        if (assert) {
-            console.log("Test passed");
-        } else {
-            console.error("Test failed");
-        }
-    }
-    */
-    assert ({assert}) {
-        
-        return (assert);
-      }
+     assert ({assert}) {
+      
+      return (assert);
+  }
 
-    inspect(args) {
-        // Implement your code to retrieve information from the system under test
-        // For demonstration purposes, we'll return a sample value
-        const balance =100;
-        return balance;
-    }
+  inspect ({inspect}) {
+      
+    return (inspect);
+}
 }
 
 module.exports = Scratch3YourExtension;
